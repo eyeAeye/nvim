c2f5d7eb3c7bc2d990ec43a66757ff14e17f27e8
Janghyun Yoo <setters_dibs_8o@icloud.com>
2025-02-19 15:10:55 +0900

    Changed noice option
    bottom_search = false
    no classic cmdline

--- config/plugins/noice.lua
+++ config/plugins/noice.lua
@@ -9,7 +9,7 @@ require("noice").setup({
 	},
 	-- you can enable a preset for easier configuration
 	presets = {
-		bottom_search = true, -- use a classic bottom cmdline for search
+		bottom_search = false, -- use a classic bottom cmdline for search
 		command_palette = true, -- position the cmdline and popupmenu together
 		long_message_to_split = true, -- long messages will be sent to a split
 		inc_rename = false, -- enables an input dialog for inc-rename.nvim
@@ -22,8 +22,8 @@ require("noice").setup({
 			filter = { event = "msg_showmode" },
 		},
 	},
-	-- classic commandline view
-	cmdline = {
-		view = "cmdline",
-	},
+	-- -- classic commandline view
+	-- cmdline = {
+	-- 	view = "cmdline",
+	-- },
 })
